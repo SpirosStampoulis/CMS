58d278013aeccae5711c42bf511e6a1ab97716b5
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 12:41:25 +0100

    Creating creating navigation links in admin (96)

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,4 @@
-<?php include "includes/header.php"?>
+<?php include "includes/header.php";?>
 
     <div id="wrapper">
 
@@ -28,4 +28,5 @@
         </div>
         <!-- /#page-wrapper -->
 
-   <? include "includes/footer.php" ?>
+   <?php include "includes/footer.php"; ?>
+
