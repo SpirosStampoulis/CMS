b1d7096b5b339eac43ffa5cd37fdd2916719d759
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 14:38:51 +0100

    Updating or editing Categories(103)

--- admin/categories.php
+++ admin/categories.php
@@ -41,6 +41,28 @@
                             <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                         </div>
                     </form>
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="cat_title">Edit Category</label>
+                                <?php
+                                    if(isset($_GET['edit'])) {
+                                        $cat_id = $_GET['edit'];
+
+                                        $query = "SELECT * FROM categories where cat_id = $cat_id";
+                                        $select_categories_id = mysqli_query($connection, $query);
+                                        while ($row = mysqli_fetch_assoc($select_categories_id)) {
+                                            $cat_id = $row['cat_id'];
+                                            $cat_title = $row['cat_title'];
+                                            ?>
+                                            <input value="<?php if(isset($cat_title)) {echo $cat_title;} ?>" class="form-control" type="text" name="cat_title">
+
+                                        <?php }} ?>
+
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Update">
+                            </div>
+                        </form>
                     </div>
                     <div class="col-xs-6">
                         <table class="table table-bordered table-hover">
@@ -62,6 +84,7 @@
                                         echo "<td>{$cat_id}</td>";
                                         echo "<td>{$cat_title}</td>";
                                         echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                                        echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
                                         echo "</tr>";
                                     }?>
                                 </tr>
