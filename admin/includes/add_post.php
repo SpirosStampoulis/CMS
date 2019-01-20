502382a0da138b70fca7bfcc102d70f49cfef8bc
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 18:51:25 +0100

    Insert Post Data from Admin (111)

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -3,33 +3,33 @@
    
 
    if(isset($_POST['create_post'])) {
-   
-            $post_title        = escape($_POST['title']);
-            $post_user         = escape($_POST['post_user']);
-            $post_category_id  = escape($_POST['post_category']);
-            $post_status       = escape($_POST['post_status']);
-    
-            $post_image        = escape($_FILES['image']['name']);
-            $post_image_temp   = escape($_FILES['image']['tmp_name']);
-    
-    
-            $post_tags         = escape($_POST['post_tags']);
-            $post_content      = escape($_POST['post_content']);
-            $post_date         = escape(date('d-m-y'));
+
+            $post_title        = $_POST['title'];
+            $post_user         = $_POST['post_user'];
+            $post_category_id  = $_POST['post_category'];
+            $post_status       = $_POST['post_status'];
+
+            $post_image        = $_FILES['image']['name'];
+            $post_image_temp   = $_FILES['image']['tmp_name'];
+
+
+            $post_tags         = $_POST['post_tags'];
+            $post_content      = $_POST['post_content'];
+            $post_date         = date('d-m-y');
             $post_comment_count = 4;
 
-       
+
         move_uploaded_file($post_image_temp, "../images/$post_image" );
-       
-       
-//      $query = "INSERT INTO posts(post_category_id, post_title, post_user, post_date,post_image,post_content,post_tags,post_status) ";
-//
-//      $query .= "VALUES({$post_category_id},'{$post_title}','{$post_user}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') ";
-//
-//      $create_post_query = mysqli_query($connection, $query);
-//
-//      confirmQuery($create_post_query);
-//
+
+
+      $query = "INSERT INTO posts(post_category_id, post_title, post_user, post_date,post_image,post_content,post_tags,post_status) ";
+
+      $query .= "VALUES({$post_category_id},'{$post_title}','{$post_user}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') ";
+
+      $create_post_query = mysqli_query($connection, $query);
+
+      confirmQuery($create_post_query);
+
 //      $the_post_id = mysqli_insert_id($connection);
 //
 //
