f094fd7f4774dc56cb734d5d3d68425f248ef8a3
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 22:19:16 +0100

    RelatingCategoriesto posts and displaying it (117)

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -31,7 +31,16 @@
         echo "<td>{$post_id}</td>";
         echo "<td>{$post_author}</td>";
         echo "<td>{$post_title}</td>";
-        echo "<td>{$post_category_id}</td>";
+
+        $query = "SELECT * FROM categories where cat_id = $post_category_id";
+            $select_categories_id = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($select_categories_id)) {
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+                echo "<td>{$cat_title}</td>";
+
+            }
+
         echo "<td>{$post_status}</td>";
         echo "<td ><img class='img-responsive' src='../images/$post_image' </td>";
         echo "<td>{$post_tags}</td>";
