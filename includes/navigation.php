7668682dd24bd1f4cb7f154ae496ab1795072f49
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 11:58:23 +0100

    Creating a reusable code for the admin (95)

--- includes/navigation.php
+++ includes/navigation.php
@@ -20,9 +20,9 @@
                     $cat_title = $row['cat_title'];
                     echo "<li><a href='#'>{$cat_title}</a></li>";
                 }?>
-<!--                <li>-->
-<!--                    <a href="#">About</a>-->
-<!--                </li>-->
+                <li>
+                    <a href="admin">Admin</a>
+                </li>
 <!--                <li>-->
 <!--                    <a href="#">Services</a>-->
 <!--                </li>-->
