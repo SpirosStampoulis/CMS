c100db708ff28e5567e89c8716d6473546f2b6ae
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 09:42:59 +0100

    Creating the posts table (87)

--- includes/navigation.php
+++ includes/navigation.php
@@ -15,8 +15,8 @@
             <ul class="nav navbar-nav">
                 <?php
                 $query = "SELECT * FROM categories ";
-                $select_al_categories_query = mysqli_query($connection, $query);
-                while($row = mysqli_fetch_assoc($select_al_categories_query)) {
+                $select_all_categories_query = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_assoc($select_all_categories_query)) {
                     $cat_title = $row['cat_title'];
                     echo "<li><a href='#'>{$cat_title}</a></li>";
                 }?>
