9d3a121181426c423d1b78db6867cd813239e13c
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 22:02:06 +0100

    Updating Posts (116)

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,20 +1,61 @@
 <?php
+
 if(isset($_GET['p_id'])){
-    $the_post_id = $_GET['p_id'];
+
+    $the_post_id =  $_GET['p_id'];
+
 }
-$query = "SELECT * FROM posts ";
-$select_posts_by_id = mysqli_query($connection, $query);
+
+
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+$select_posts_by_id = mysqli_query($connection,$query);
+
 while($row = mysqli_fetch_assoc($select_posts_by_id)) {
-$post_id = $row['post_id'];
-$post_author = $row['post_author'];
-$post_title = $row['post_title'];
-$post_category_id = $row['post_category_id'];
-$post_status = $row['post_status'];
-$post_image = $row['post_image'];
-$post_content = $row['post_content'];
-$post_tags = $row['post_tags'];
-$post_comment_count = $row['post_comment_count'];
-$post_date = $row['post_date'];
+    $post_id            = $row['post_id'];
+    $post_author         = $row['post_author'];
+    $post_title         = $row['post_title'];
+    $post_category_id   = $row['post_category_id'];
+    $post_status        = $row['post_status'];
+    $post_image         = $row['post_image'];
+    $post_content       = $row['post_content'];
+    $post_tags          = $row['post_tags'];
+    $post_comment_count = $row['post_comment_count'];
+    $post_date          = $row['post_date'];
+}
+if(isset($_POST['update_post'])){
+    $post_title        = $_POST['post_title'];
+    $post_author         = $_POST['post_author'];
+    $post_category_id  = $_POST['post_category'];
+    $post_status       = $_POST['post_status'];
+    $post_image        = $_FILES['image']['name'];
+    $post_image_temp   = $_FILES['image']['tmp_name'];
+    $post_content        = $_POST['post_content'];
+    $post_tags         = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image" );
+    if(empty($post_image)) {
+        $query = "SELECT * from posts where post_id = $the_post_id ";
+        $select_image = mysqli_query($connection, $query);
+        while ($row = mysqli_fetch_assoc($select_image)){
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_category_id = '{$post_category_id}', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags= '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "where post_id = {$the_post_id} ";
+
+    $update_post = mysqli_query($connection, $query);
+    confirmQuery($update_post);
+
+
 }
 ?>
 <form action="" method="post" enctype="multipart/form-data">
@@ -22,12 +63,11 @@ $post_date = $row['post_date'];
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input value="<?php echo $post_title;?>"type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
-        <label for="category">Post Category</label>
-        <select>
+        <select name="post_category" id="">
             <?php
             $query = "SELECT * FROM categories";
             $select_categories = mysqli_query($connection, $query);
@@ -56,7 +96,8 @@ $post_date = $row['post_date'];
 
     <div class="form-group">
         <label for="category">Post Image</label>
-        <img class="img-responsive" src="../images/<?php echo $post_image?>"
+        <img class="img-responsive" src="../images/<?php echo $post_image; ?>"
+        <input type="file" name="image">
     </div>
 
     <div class="form-group">
@@ -72,7 +113,7 @@ $post_date = $row['post_date'];
 
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
 
 
