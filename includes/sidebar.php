161fbbc57f0c68d135808ed1ae30f271ace15cad
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 11:04:42 +0100

    Create a custom search engine (93)

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,25 +1,9 @@
 <div class="col-md-4">
-    <?php
-    if(isset($_POST['submit'])){
-        $search = $_POST['search'];
-        $query = "SELECT * FROM posts where post_tags like '%$search%'";
-        $search_query = mysqli_query($connection, $query);
-        if(!$search_query){
-            die("QUERY FAILED" . mysqli_error($connection));
-        }
-        $count = mysqli_num_rows($search_query);
-        if($count == 0){
-            echo "<h1>NO RESULT</h1>";
-        }else{
-            echo "some result";
-        }
-    }
 
-    ?>
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <form action="" method="post">
+        <form action="search.php" method="post">
         <div class="input-group">
             <input name="search" type="text" class="form-control">
             <span class="input-group-btn">
