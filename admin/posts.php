e6427e0d5bff283fd632a8db45dd4bb1bc8fb6a0
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-20 18:02:39 +0100

    Displaying posts in admin(108)

--- admin/posts.php
+++ admin/posts.php
@@ -32,7 +32,32 @@
                         </thead>
 
                     <tbody>
-                    <tr>
+                    <?php
+                    $query = "SELECT * FROM posts ";
+                    $select_posts = mysqli_query($connection, $query);
+                    while($row = mysqli_fetch_assoc($select_posts)) {
+                        $post_id = $row['post_id'];
+                        $post_author = $row['post_author'];
+                        $post_title = $row['post_title'];
+                        $post_category_id = $row['post_category_id'];
+                        $post_status = $row['post_status'];
+                        $post_image = $row['post_image'];
+                        $post_tags = $row['post_tags'];
+                        $post_comment_count = $row['post_comment_count'];
+                        $post_date = $row['post_date'];
+                        echo "<tr>";
+                        echo "<td>{$post_id}</td>";
+                        echo "<td>{$post_author}</td>";
+                        echo "<td>{$post_title}</td>";
+                        echo "<td>{$post_category_id}</td>";
+                        echo "<td>{$post_status}</td>";
+                        echo "<td ><img class='img-responsive' src='../images/$post_image' </td>";
+                        echo "<td>{$post_tags}</td>";
+                        echo "<td>{$post_comment_count}</td>";
+                        echo "<td>{$post_date}</td>";
+                        echo "</tr>";
+                    }
+                    ?>
                         <td>10</td>
                         <td>Edwin Diaz</td>
                         <td>Bootstrap framework</td>
@@ -42,7 +67,7 @@
                         <td>tags</td>
                         <td>comments</td>
                         <td>date</td>
-                    </tr>
+
                     </tbody>
                     </table>
                 </div>
--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo "$post_date"?></p>
                 <hr>
-                <img class="img-responsive" src="/images/<?php echo "$post_image"?>" alt="">
+                <img class='img-responsive' src='images/<?php echo $post_image?>' alt="">
                 <hr>
                 <p><?php echo "$post_content"?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
