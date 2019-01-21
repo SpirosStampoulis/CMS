6cceac541b3abc07a26e5c953972165d21f2c0d3
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-21 23:03:37 +0100

    Adding individual post and link (119)

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
                 $select_all_posts_query = mysqli_query($connection, $query);
                 while($row = mysqli_fetch_assoc($select_all_posts_query)) {
                     $post_title = $row['post_title'];
+                    $post_id = $row['post_id'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
@@ -29,7 +30,7 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo "$post_title"?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo "$post_title"?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo "$post_author"?></a>
