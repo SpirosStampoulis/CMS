84120799617e89d12cd22ceb758bda4d8068b8d9
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-21 22:40:03 +0100

    Adding categories to add post site and fixes (118)

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -5,13 +5,12 @@
    if(isset($_POST['create_post'])) {
 
             $post_title        = $_POST['title'];
-            $post_user         = $_POST['post_user'];
+            $post_author        = $_POST['author'];
             $post_category_id  = $_POST['post_category'];
             $post_status       = $_POST['post_status'];
 
             $post_image        = $_FILES['image']['name'];
             $post_image_temp   = $_FILES['image']['tmp_name'];
-
 
             $post_tags         = $_POST['post_tags'];
             $post_content      = $_POST['post_content'];
@@ -22,105 +21,50 @@
         move_uploaded_file($post_image_temp, "../images/$post_image" );
 
 
-      $query = "INSERT INTO posts(post_category_id, post_title, post_user, post_date,post_image,post_content,post_tags,post_status) ";
+      $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date,post_image,post_content,post_tags,post_status) ";
 
-      $query .= "VALUES({$post_category_id},'{$post_title}','{$post_user}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') ";
+      $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') ";
 
       $create_post_query = mysqli_query($connection, $query);
 
       confirmQuery($create_post_query);
 
-//      $the_post_id = mysqli_insert_id($connection);
-//
-//
-//      echo "<p class='bg-success'>Post Created. <a href='../post.php?p_id={$the_post_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
-//
-
-
    }
-
-
-
 
 ?>
 
-    <form action="" method="post" enctype="multipart/form-data">    
-     
-     
+    <form action="" method="post" enctype="multipart/form-data">
+
       <div class="form-group">
          <label for="title">Post Title</label>
           <input type="text" class="form-control" name="title">
       </div>
 
          <div class="form-group">
-       <label for="category">Category</label>
        <select name="post_category" id="">
-           
+
 <?php
 
-//        $query = "SELECT * FROM categories";
-//        $select_categories = mysqli_query($connection,$query);
-//
-//        confirmQuery($select_categories);
-//
-//
-//        while($row = mysqli_fetch_assoc($select_categories )) {
-//        $cat_id = $row['cat_id'];
-//        $cat_title = $row['cat_title'];
-//
-//
-//            echo "<option value='$cat_id'>{$cat_title}</option>";
-//
-//
-//        }
+        $query = "SELECT * FROM categories";
+        $select_categories = mysqli_query($connection,$query);
 
+        confirmQuery($select_categories);
+
+        while($row = mysqli_fetch_assoc($select_categories )) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+
+            echo "<option value='$cat_id'>{$cat_title}</option>";
+
+        }
 ?>
-           
-        
        </select>
-      
       </div>
 
-
-       <div class="form-group">
-       <label for="users">Users</label>
-       <select name="post_user" id="">
-           
-<?php
-
-//        $users_query = "SELECT * FROM users";
-//        $select_users = mysqli_query($connection,$users_query);
-//
-//        confirmQuery($select_users);
-//
-//
-//        while($row = mysqli_fetch_assoc($select_users)) {
-//        $user_id = $row['user_id'];
-//        $username = $row['username'];
-//
-//
-//            echo "<option value='{$username}'>{$username}</option>";
-//
-//
-//        }
-
-?>
-
-        
-       </select>
-      
-      </div>
-
-
-
-
-
-      <!-- <div class="form-group">
+        <div class="form-group">
          <label for="title">Post Author</label>
           <input type="text" class="form-control" name="author">
-      </div> -->
-      
-      
+      </div>
 
        <div class="form-group">
          <select name="post_status" id="">
@@ -129,13 +73,12 @@
              <option value="draft">Draft</option>
          </select>
       </div>
-      
-      
-      
-    <div class="form-group">
-         <label for="post_image">Post Image</label>
-          <input type="file"  name="image">
-      </div>
+
+        <div class="form-group">
+            <label for="category">Post Image</label>
+            <img class="img-responsive" src="../images/<?php echo $post_image; ?>">
+            <input type="file" name="image">
+        </div>
 
       <div class="form-group">
          <label for="post_tags">Post Tags</label>
@@ -147,13 +90,10 @@
          <textarea class="form-control "name="post_content" id="" cols="30" rows="10">
          </textarea>
       </div>
-      
-      
 
        <div class="form-group">
           <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
       </div>
-
 
 </form>
     
