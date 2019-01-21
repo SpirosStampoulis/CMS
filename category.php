0164180b443e633ed8265156c5e19b7961ec84c2
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2019-01-21 23:22:57 +0100

    Making an excerpt (121)

--- category.php
+++ category.php
@@ -25,7 +25,7 @@
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];
+                    $post_content = substr($row['post_content'],0,90);
                     ?>
 
                 <h1 class="page-header">
